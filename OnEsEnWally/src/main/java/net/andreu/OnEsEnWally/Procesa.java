0692dccdc4948af365e21a36568468995b44c8bf
Andreu <Andreu@Andreu1>
2016-11-21 10:54:23 +0100

    Finally

--- OnEsEnWally/src/main/java/net/andreu/OnEsEnWally/Procesa.java
+++ OnEsEnWally/src/main/java/net/andreu/OnEsEnWally/Procesa.java
@@ -1,31 +1,28 @@
 package net.andreu.OnEsEnWally;
 
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.annotation.processing.FilerException;
-
 public class Procesa {
-
-	List<Integer> resultats = new ArrayList<>();
 	
-	public void procesaFitxer(String linia, int n_linia) throws FilerException {
-		
-		String paraula = "Wally";
+	String paraula = "Wally";
+	int trovat = 0;
+	
+	public void procesaFitxer(String linia, int n_linia){
 		
 		if (linia.indexOf("Wally") != -1) {
+			trovat ++;
 			Pattern p = Pattern.compile("(([\\s|\\\\.|\\\\,]" + paraula + ")|^" + paraula + ")([\\s|\\\\.|\\\\,]|$)");
 			@SuppressWarnings("unused")
 			Matcher m = p.matcher(linia);
 			
-	        if (resultats.size() > 1) {
-	        	throw new FilerException("FITXER INCORRECTE");
+	        if (trovat > 1) {
+	        	System.out.println("FITXER INCORRECTE");
 	        }else{
 	        	System.out.println("En Wally esta en la linia " + n_linia);
-	        }	
+	        }
+			
 		} else {
 			System.out.println("No hi ha en Wally");
 		}
